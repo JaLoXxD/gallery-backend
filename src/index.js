b8b0b3cdf7aaae36112556e909e8b89f0711c40b
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-07-14 15:18:53 -0500

    [ADD] server config
    principal configuration

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); //READ JSON
+app.use(cors());
+require("dotenv").config();
+const { API_PORT } = process.env;
+const port = process.env.PORT || API_PORT;
+
+app.listen(app.get("port"), () => {
+	console.log("server listen on port " + app.get("port") + "...");
+});
+
+app.set("port", port);
