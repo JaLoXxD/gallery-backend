a33437dc62461e6d2ba52999f35651db3c82e400
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-07-14 16:40:12 -0500

    [ADD] routes and middleware
    add image routes and verifyToken middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,11 @@ const { registerUser, login, resetPassword } = require("../controllers/user");
 
 router.post("/register", registerUser);
 router.post("/login", login);
+router.post("/uploadImage", verifyToken, uploadImage);
+router.get("/getUserImages", verifyToken, getUserImages);
 router.post("/resetPassword", resetPassword);
+router.get("/uploads", getImageURL);
+router.post("/deleteImage", verifyToken, deleteImage);
+router.put("/updateImage", verifyToken, updateImage);
 
 module.exports = router;
